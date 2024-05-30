f23b02d387ce703f7867fe04656f789406ee782d
salmanjahani1 <163454198+salmanjahani1@users.noreply.github.com>
2024-05-30 10:17:37 -0700

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,4 +1,4 @@
 def main(event, context):
-    message = "Hello World from the Kyma Function "+context['function-name']+" running on "+context['runtime']+ "created by Salman!";
+    message = "Hello World from the Kyma Function "+context['function-name']+" running on "+context['runtime']+ " created by Salman! (Git version)";
     print(message)
     return message
